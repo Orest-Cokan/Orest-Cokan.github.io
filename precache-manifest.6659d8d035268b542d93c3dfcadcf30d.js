bb02fe414357246a37dba43715dde3e2687b9718
skryt <cokan@ualberta.ca>
2019-02-11 00:17:39 -0700

    Updates

---
+++ precache-manifest.6659d8d035268b542d93c3dfcadcf30d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "414a58b934145564a7ad",
+    "url": "/Orest-Cokan.github.io/static/css/main.d20c0109.chunk.css"
+  },
+  {
+    "revision": "414a58b934145564a7ad",
+    "url": "/Orest-Cokan.github.io/static/js/main.414a58b9.chunk.js"
+  },
+  {
+    "revision": "947d507a2eb31f2f56b4",
+    "url": "/Orest-Cokan.github.io/static/js/1.947d507a.chunk.js"
+  },
+  {
+    "revision": "4598d6524ca266a9f974",
+    "url": "/Orest-Cokan.github.io/static/js/runtime~main.4598d652.js"
+  },
+  {
+    "revision": "45160001973f6024ebad0822c5edc3eb",
+    "url": "/Orest-Cokan.github.io/index.html"
+  }
+];
