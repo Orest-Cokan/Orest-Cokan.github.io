ecdd9768dc23646b584d0205f7ee5361cab2d353
skryt <cokan@ualberta.ca>
2019-02-24 23:02:20 -0700

    Updates

---
+++ precache-manifest.10c9dcf6f1cbf08f93d75ac58487db89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1294b16757398c1224c9",
+    "url": "/relearn-destiny.github.io/static/css/main.7246f537.chunk.css"
+  },
+  {
+    "revision": "1294b16757398c1224c9",
+    "url": "/relearn-destiny.github.io/static/js/main.1294b167.chunk.js"
+  },
+  {
+    "revision": "2ff99bcc320aa5d51459",
+    "url": "/relearn-destiny.github.io/static/js/1.2ff99bcc.chunk.js"
+  },
+  {
+    "revision": "7e40ecec1edce0a8197b",
+    "url": "/relearn-destiny.github.io/static/js/runtime~main.7e40ecec.js"
+  },
+  {
+    "revision": "3648df091b041b7f099a984aa0dc5837",
+    "url": "/relearn-destiny.github.io/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Orest-Cokan.github.io/precache-manifest.ac5d447b0f6ee78e7fa92d1f5f7e75c9.js"
+  "/relearn-destiny.github.io/precache-manifest.10c9dcf6f1cbf08f93d75ac58487db89.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Orest-Cokan.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/relearn-destiny.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
